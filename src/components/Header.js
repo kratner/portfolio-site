710fdc1e5fc23dcc11439a6d8deedc5621696c70
Keith Ratner <keith.ratner@gmail.com>
2023-05-06 13:24:19 -0700

    Nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,36 @@ function Header() {
       {/* <div className="kr">Keith Ratner</div> */}
       <nav>
         <ul className="nav">
-          {/* <li>Home</li>
-          <li>About</li>
-          <li>Contact</li> */}
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://keithratner.live/shop/"
+              title="Shop for My Artwork!"
+            >
+              Store
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://optionexplicit.live"
+              title="Visit My VBA Code Library"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://keithratner.live/about/#contactme"
+              title="Contact Me"
+            >
+              Contact
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
