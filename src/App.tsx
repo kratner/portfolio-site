6a4132de04d337bf8b1683470a413176b91e28e2
Keith Ratner <keith.ratner@gmail.com>
2023-06-20 09:44:09 -0700

    SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./styles/main.scss";
 import React from "react";
 import Header from "./components/wrappers/Header";
 import Footer from "./components/wrappers/Footer";
-import CrossFadeImages from "./components/wrappers/CrossFadeImages";
+// import CrossFadeImages from "./components/wrappers/CrossFadeImages";
 import AnimatedSVG from "./components/AnimatedSVG";
 
 const App = () => {
@@ -12,8 +12,13 @@ const App = () => {
     <div>
       <Header />
       <main>
-      {/* <AnimatedSVG svgPath="../assets/images/moeba-white.svg" targetElement="path" /> */}
-        <AnimatedSVG svgPath="https://kratner.github.io/portfolio-site/images/moeba-white.svg" targetElements={["path"]} />
+        {/* <AnimatedSVG svgPath="../assets/images/moeba-white.svg" targetElement="path" /> */}
+        <AnimatedSVG 
+          svgPath="https://kratner.github.io/portfolio-site/images/moeba-white.svg" 
+          targetElements={["path"]} 
+          fadeInDuration="1s"
+          fadeOutDuration=".75s"
+        />
         {/* <CrossFadeImages />         */}
       </main>
       <Footer />
