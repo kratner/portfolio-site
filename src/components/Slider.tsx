080b5cb18631a483da3d81d0f0c0797e8a467820
Keith Ratner <keith.ratner@gmail.com>
2023-08-24 14:41:50 -0700

    Slider fixes

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -51,105 +51,172 @@ const Slider: React.FC<SliderProps> = ({
   }
 
   return (
-    <div className="slider">
-      {sliderHeading && (
-        <div className="slider-heading">
-          <h2>{sliderHeading}</h2>
+    <React.Fragment>
+      <div className="slider">
+        {sliderHeading && (
+          <div className="slider-heading">
+            <h2>{sliderHeading}</h2>
+          </div>
+        )}
+        {sliderSubheading && (
+          <div className="slider-subheading">
+            <h3>{sliderSubheading}</h3>
+          </div>
+        )}
+        <div className="slide-navigation">
+          <div className="arrow prev" onClick={goToPrevious}>
+            <span>
+              <FontAwesomeIcon icon={faCircleLeft} />
+            </span>
+          </div>
+          <div className="arrow next" onClick={goToNext}>
+            <span>
+              <FontAwesomeIcon icon={faCircleRight} />
+            </span>
+          </div>
         </div>
-      )}
-      {sliderSubheading && (
-        <div className="slider-subheading">
-          <h3>{sliderSubheading}</h3>
+        <div className="slides-all">
+          <div className="shadow-slides">
+            {slides.map((slide, index) => {
+              return (
+                <div
+                  className={
+                    index === currentSlide
+                      ? "slide-container active"
+                      : "slide-container"
+                  }
+                  key={index}
+                >
+                  <div className="slide">
+                    {slide.imgSrc && (
+                      <div className="main-image">
+                        <img
+                          src={(PUBLIC_URL ?? "") + slide.imgSrc}
+                          alt={slide.imgAltText ? slide.imgAltText : ""}
+                        />
+                      </div>
+                    )}
+                    {slide.heading && (
+                      <div className="slide-heading">
+                        <h2>{slide.heading}</h2>
+                      </div>
+                    )}
+                    {slide.subheading && (
+                      <div className="slide-subheading">
+                        <h3>{slide.subheading}</h3>
+                      </div>
+                    )}
+                    {slide.bodyText && (
+                      <div className="slide-bodyText">
+                        <p>{slide.bodyText}</p>
+                      </div>
+                    )}
+                    {slide.avatarSrc && (
+                      <div className="slide-avatar">
+                        <img
+                          src={(PUBLIC_URL ?? "") + slide.avatarSrc}
+                          alt={slide.avatarAltText ? slide.avatarAltText : ""}
+                        />
+                      </div>
+                    )}
+                    {slide.avatarHeading && (
+                      <div className="slide-avatar-heading">
+                        <h2>{slide.avatarHeading}</h2>
+                      </div>
+                    )}
+                    {slide.avatarSubheading && (
+                      <div className="slide-avatar-subheading">
+                        <h3>{slide.avatarSubheading}</h3>
+                      </div>
+                    )}
+                    {slide.avatarBodyText && (
+                      <div className="slide-avatar-subheading">
+                        <h3>{slide.avatarBodyText}</h3>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="slides">
+            {slides.map((slide, index) => {
+              return (
+                <div
+                  className={
+                    index === currentSlide
+                      ? "slide-container active"
+                      : "slide-container"
+                  }
+                  key={index}
+                >
+                  <div className="slide">
+                    {slide.imgSrc && (
+                      <div className="main-image">
+                        <img
+                          src={(PUBLIC_URL ?? "") + slide.imgSrc}
+                          alt={slide.imgAltText ? slide.imgAltText : ""}
+                        />
+                      </div>
+                    )}
+                    {slide.heading && (
+                      <div className="slide-heading">
+                        <h2>{slide.heading}</h2>
+                      </div>
+                    )}
+                    {slide.subheading && (
+                      <div className="slide-subheading">
+                        <h3>{slide.subheading}</h3>
+                      </div>
+                    )}
+                    {slide.bodyText && (
+                      <div className="slide-bodyText">
+                        <p>{slide.bodyText}</p>
+                      </div>
+                    )}
+                    {slide.avatarSrc && (
+                      <div className="slide-avatar">
+                        <img
+                          src={(PUBLIC_URL ?? "") + slide.avatarSrc}
+                          alt={slide.avatarAltText ? slide.avatarAltText : ""}
+                        />
+                      </div>
+                    )}
+                    {slide.avatarHeading && (
+                      <div className="slide-avatar-heading">
+                        <h2>{slide.avatarHeading}</h2>
+                      </div>
+                    )}
+                    {slide.avatarSubheading && (
+                      <div className="slide-avatar-subheading">
+                        <h3>{slide.avatarSubheading}</h3>
+                      </div>
+                    )}
+                    {slide.avatarBodyText && (
+                      <div className="slide-avatar-subheading">
+                        <h3>{slide.avatarBodyText}</h3>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      )}
-      <div className="slide-navigation">
-        <div className="arrow prev" onClick={goToPrevious}>
-          <span>
-            <FontAwesomeIcon icon={faCircleLeft} />
-          </span>
-        </div>
-        <div className="arrow next" onClick={goToNext}>
-          <span>
-            <FontAwesomeIcon icon={faCircleRight} />
-          </span>
+        <div className="slide-position">
+          {slides.map((_, index) => {
+            return (
+              <div
+                className={
+                  "indicator" + (index === currentSlide ? " active" : "")
+                }
+                key={index}
+              />
+            );
+          })}
         </div>
       </div>
-      <div className="slides">
-        {slides.map((slide, index) => {
-          return (
-            <div
-              className={
-                index === currentSlide
-                  ? "slide-container active"
-                  : "slide-container"
-              }
-              key={index}
-            >
-              <div className="slide">
-                {slide.imgSrc && (
-                  <div className="main-image">
-                    <img
-                      src={(PUBLIC_URL ?? "") + slide.imgSrc}
-                      alt={slide.imgAltText ? slide.imgAltText : ""}
-                    />
-                  </div>
-                )}
-                {slide.heading && (
-                  <div className="slide-heading">
-                    <h2>{slide.heading}</h2>
-                  </div>
-                )}
-                {slide.subheading && (
-                  <div className="slide-subheading">
-                    <h3>{slide.subheading}</h3>
-                  </div>
-                )}
-                {slide.bodyText && (
-                  <div className="slide-bodyText">
-                    <p>{slide.bodyText}</p>
-                  </div>
-                )}
-                {slide.avatarSrc && (
-                  <div className="slide-avatar">
-                    <img
-                      src={(PUBLIC_URL ?? "") + slide.avatarSrc}
-                      alt={slide.avatarAltText ? slide.avatarAltText : ""}
-                    />
-                  </div>
-                )}
-                {slide.avatarHeading && (
-                  <div className="slide-avatar-heading">
-                    <h2>{slide.avatarHeading}</h2>
-                  </div>
-                )}
-                {slide.avatarSubheading && (
-                  <div className="slide-avatar-subheading">
-                    <h3>{slide.avatarSubheading}</h3>
-                  </div>
-                )}
-                {slide.avatarBodyText && (
-                  <div className="slide-avatar-subheading">
-                    <h3>{slide.avatarBodyText}</h3>
-                  </div>
-                )}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="slide-position">
-        {slides.map((_, index) => {
-          return (
-            <div
-              className={
-                "indicator" + (index === currentSlide ? " active" : "")
-              }
-              key={index}
-            />
-          );
-        })}
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
