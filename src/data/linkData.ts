15b68e0ac306ec2ca512c8319661bd72126d1045
Keith Ratner <keith.ratner@gmail.com>
2024-06-01 09:17:59 -0500

    Import as type

--- src/data/linkData.ts
+++ src/data/linkData.ts
@@ -1,5 +1,5 @@
 // linkData.ts
-import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import {
   faLinkedin,
   faYoutube,
